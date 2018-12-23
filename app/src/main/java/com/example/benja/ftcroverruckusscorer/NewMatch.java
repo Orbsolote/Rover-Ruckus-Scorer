68eb0d2df87f7835a5ef8323624beba2311dcc24
Orbsolote <benjaminmuellerusa@yahoo.com>
2018-12-22 20:31:30 -0500

    Test 1

--- app/src/main/java/com/example/benja/ftcroverruckusscorer/NewMatch.java
+++ app/src/main/java/com/example/benja/ftcroverruckusscorer/NewMatch.java
@@ -1,14 +1,14 @@
 package com.example.benja.ftcroverruckusscorer;
 
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.widget.EditText;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
 
 public class NewMatch extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new_match);
+        setContentView(R.layout.activity__new__match);
     }
 }
