2f368eeff9f110398ff28c93ae918f0bb5e930e8
Orbsolote <benjaminmuellerusa@yahoo.com>
2018-12-23 00:42:00 -0500

    Majority of the Homepage completed, New Match Activity Started Organized.

--- app/src/main/java/com/example/benja/ftcroverruckusscorer/NewMatch.java
+++ app/src/main/java/com/example/benja/ftcroverruckusscorer/NewMatch.java
@@ -1,14 +1,31 @@
 package com.example.benja.ftcroverruckusscorer;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.EditText;
+import android.view.View;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class NewMatch extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity__new__match);
+        setContentView(R.layout.activity_new_match);
+    }
+
+    public void Alliance_Color(View view){
+        Switch AllianceColor = (Switch)findViewById(R.id.AllianceColorSwitch);
+        TextView Points = (TextView)findViewById(R.id.Points);
+
+        if (AllianceColor.isChecked()) {
+            Points.setBackgroundColor(Color.RED);
+        }
+        else{
+            Points.setBackgroundColor(Color.BLUE);
+        }
     }
 }
