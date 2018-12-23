202449dccc679f9ce573474174d30928ef5068ba
Orbsolote <benjaminmuellerusa@yahoo.com>
2018-12-22 19:13:28 -0500

    Initial commit

---
+++ app/src/main/java/com/example/benja/ftcroverruckusscorer/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.benja.ftcroverruckusscorer;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openNewMatch(View view){
+        Intent startNewMatchActivity = new Intent(this, NewMatch.class);
+        startActivity(startNewMatchActivity);
+    }
+}
